c14bd55622a27924fab51bfde0166bb955f6b975
aagrah7 <50784368+aagrah7@users.noreply.github.com>
2024-06-07 19:47:11 -0400

    updates

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://gorgeous-heron-93.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
