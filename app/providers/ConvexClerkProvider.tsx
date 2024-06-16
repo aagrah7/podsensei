6df77b65c3a66c5b7225d878f25674d6c2447998
aagrah7 <50784368+aagrah7@users.noreply.github.com>
2024-06-16 19:16:44 -0400

    updates

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -1,13 +1,26 @@
 "use client";
 import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
-  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+  appearance={{
+    layout: { 
+      socialButtonsVariant: 'iconButton',
+    logoImageUrl: '/icons/auth-logo.svg'
+  },
+  variables: {
+    colorBackground: '#15171c',
+    colorPrimary: '',
+    colorText: 'white',
+    colorInputBackground: '#1b1f29',
+    colorInputText: 'white',
+  }
+  }}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as http from "../http.js";
+import type * as openai from "../openai.js";
 import type * as tasks from "../tasks.js";
 import type * as users from "../users.js";
 
@@ -26,6 +28,8 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  http: typeof http;
+  openai: typeof openai;
   tasks: typeof tasks;
   users: typeof users;
 }>;
